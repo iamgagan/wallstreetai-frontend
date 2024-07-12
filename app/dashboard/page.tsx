5f91422fbd52178a058830f66f5f6d805a0096f3
Francis_ldn <fnhh04@gmail.com>
2024-07-12 15:50:30 +0100

    add dashboard button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { MenuButton } from "@/components/MenuButton/MenuButton";
+import { Button } from "@/components/ui/button";
 
 const menuItemList = [
   "resume generator",
@@ -11,6 +12,13 @@ const menuItemList = [
 export default function Dashboard() {
   return (
     <main className='flex min-h-screen flex-col items-center p-24 w-full'>
+      <Button
+        variant='outline'
+        size='default'
+        className='absolute top-8 left-8 bg-transparent text-white rounded-xl font-semibold'
+      >
+        dashboard
+      </Button>
       <div className='fixed right-0 flex flex-col gap-6'>
         {menuItemList.map((item) => (
           <MenuButton key={item} buttonLabel={item} />
