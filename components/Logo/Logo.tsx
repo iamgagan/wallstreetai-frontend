b4b0870c8f7a3c657e5aa6a44fb97da5bca23f18
Francis Hui <fhui@westminster.gov.uk>
2024-07-19 23:22:56 +0100

    add navigation

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface LogoProps {
   background: "light" | "dark";
 }
@@ -6,13 +8,15 @@ export const Logo = ({ background }: LogoProps) => {
   return (
     <div className='flex flex-col items-center justify-center'>
       {/* <img src='/logo.svg' alt='logo' className='w-24 h-24' /> */}
+      <Link href={"/dashboard"}>
       <h1
         className={`text-2xl font-bold ${
           background === "dark" ? "text-white" : "text-black"
         }`}
       >
         BreakTheWall
-      </h1>
+        </h1>
+        </Link>
     </div>
   );
 };
