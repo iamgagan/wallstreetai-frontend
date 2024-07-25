12d00a5e3720e97c014e927677114e55dda27625
Francis_ldn <fnhh04@gmail.com>
2024-07-25 12:49:34 +0100

    add api status

--- app/api/resume/[userId]/route.ts
+++ app/api/resume/[userId]/route.ts
@@ -6,9 +6,9 @@ export async function GET(req: NextRequest) {
    
     try {
         const response = await getResumesByUserId(userId);
-        return NextResponse.json({ resumes: response});
+        return NextResponse.json({ resumes: response}, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error: 'Internal Server Error' });
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 
 }
--- app/api/resume/upload/route.ts
+++ app/api/resume/upload/route.ts
@@ -12,9 +12,9 @@ export async function POST(req: NextRequest) {
     const { userId, resume, resumeFileId } = await req.json() as RequestBody;
     try {
         const response = await updateOrCreateResumeByUserId(userId, resume, resumeFileId);
-        return NextResponse.json({message: 'Resume uploaded successfully', data: response});
+        return NextResponse.json({message: 'Resume uploaded successfully', data: response}, { status: 201 });
     } catch (error) {
-        return NextResponse.json({ error: 'Internal Server Error' });
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
   
 }
