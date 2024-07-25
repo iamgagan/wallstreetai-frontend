1b0753f72b775cb6bd1a6fb475abbc8ff9f4578b
Francis_ldn <fnhh04@gmail.com>
2024-07-25 12:50:51 +0100

    edit api

--- app/api/resumeFile/[userId]/route.ts
+++ app/api/resumeFile/[userId]/route.ts
@@ -1,12 +1,12 @@
 import { getResumeFilesByUserId } from '@/lib/resume';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
     const userId = req.nextUrl.pathname;
    
     try {
         const response = await getResumeFilesByUserId(userId);
-        return Response.json({
+        return NextResponse.json({
             data: response ? response.map((resumeFile) => {
                 return {
                     id: resumeFile.id,
@@ -16,10 +16,10 @@ export async function GET(req: NextRequest) {
                     fileType: resumeFile.fileType,
                 }
             }) : null
-        });
+        }, { status: 200 });
 
     } catch (error) {
-        return Response.json({ error: 'Internal Server Error' });
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
    
 }
