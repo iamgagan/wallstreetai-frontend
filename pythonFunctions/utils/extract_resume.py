03c758f585ec706ba0a96197eb9f899093669e21
Francis_ldn <fnhh04@gmail.com>
2024-07-26 23:56:47 +0100

    fix resume data endpoint

--- pythonFunctions/utils/extract_resume.py
+++ pythonFunctions/utils/extract_resume.py
@@ -1,63 +1,20 @@
 import json
+import tempfile
 from typing import Dict, Any
 import os
-
 import requests
 from crewai import Agent, Task, Crew
 from langchain_openai import ChatOpenAI
-from langchain.tools import tool
-from langchain_community.document_loaders import PyMuPDFLoader
 from dotenv import load_dotenv
+from extract_resume_tools import download_doc, fetch_doc_content
 
-# from langchain_community.document_loaders.word_document import UnstructuredWordDocumentLoader
-# import base64
-# import tempfile
-# from document_reader import extract_text_from_pdf
 
 load_dotenv()
 # Load environment variables
 api_key = os.getenv("API_KEY")
 model = ChatOpenAI(api_key=api_key, model_name="gpt-4-turbo", temperature=0.8)
-pdf_url = 'https://res.cloudinary.com/dac48s3os/image/upload/v1722011750/Sarah_Chen_CV_yebcz4.pdf'
-
-
-@tool
-def fetch_doc_content(file_url: str) -> str:
-    """Given pdf file_url, read the file and return the content"""
-
-    # doc_data = base64.b64decode(base64_string)
-    # file_type_suffix = file_extension(file_type)
-    #
-    # # Create a temporary file
-    # with tempfile.NamedTemporaryFile(delete=False, suffix=file_type_suffix) as temp_file:
-    #     temp_path = temp_file.name
-    #     # Write the binary data to the temporary file
-    #     temp_file.write(doc_data)
-    try:
-        # if file_type_suffix == ".pdf":
-        loader = PyMuPDFLoader(file_url)
-        data = loader.load()[0]
-        return data.page_content
-
-        # elif file_type_suffix == ".docx" or file_type_suffix == ".doc":
-        #     loader = UnstructuredWordDocumentLoader(temp_path)
-        #     data = loader.load()[0]
-        #     return data.page_content
-
-    except Exception as e:
-        return f"Error reading doc: {str(e)}"
-
-@tool
-def get_webpage_contents(url: str) -> str:
-    """Reads the webpage with a given URL and returns the page content"""
-    try:
-        response = requests.get(url, verify=False)  # Disable SSL verification
-        response.raise_for_status()
-        return response.text
-    except requests.exceptions.RequestException as e:
-        return f"Error fetching webpage: {str(e)}"
-
-
+doc_url = 'https://res.cloudinary.com/dac48s3os/image/upload/v1722011750/Sarah_Chen_CV_yebcz4.pdf'
+pdf_path = 'Sarah_Chen_CV.pdf'
 
 
 personal_details_analyst = Agent(
@@ -66,7 +23,7 @@ personal_details_analyst = Agent(
          'phone number, address line1, address line2, city, postal code and country',
     backstory='Specialized in parsing document content for personal details',
     verbose=True,
-    tools=[get_webpage_contents],
+    tools=[fetch_doc_content],
     allow_delegation=False,
     llm=model
 )
@@ -77,7 +34,7 @@ work_experience_analyst = Agent(
          'company location, start date, end date, job description for all the past and present jobs',
     backstory='Specialized in parsing document content for work experience',
     verbose=True,
-    tools=[get_webpage_contents],
+    tools=[fetch_doc_content],
     allow_delegation=False,
     llm=model
 )
@@ -88,7 +45,7 @@ education_analyst = Agent(
          'field of study, start date, end date, description for all past and present education',
     backstory='Specialized in parsing document content for education',
     verbose=True,
-    tools=[get_webpage_contents],
+    tools=[fetch_doc_content],
     allow_delegation=False,
     llm=model
 )
@@ -99,15 +56,15 @@ qualifications_analyst = Agent(
          'awarding institution',
     backstory='Specialized in parsing document content for qualifications',
     verbose=True,
-    tools=[get_webpage_contents],
+    tools=[fetch_doc_content],
     allow_delegation=False,
     llm=model
 )
 
 
-def extract_personal_details(file_url: str) -> Task:
+def extract_personal_details(file_path: str) -> Task:
     return Task(
-        description=f""" Given a .pdf, .doc or .docx document provided at this url: {file_url}, read the file and extract the personal details of the applicant. Return the information as a JSON string with the following structure: 
+        description=f""" Given a .pdf, .doc or .docx document provided at this file path: {file_path}, read the file and extract the personal details of the applicant. Return the information as a JSON string with the following structure: 
         {{ 
             "firstName": "John", 
             "lastName": "Smith", 
@@ -124,9 +81,9 @@ def extract_personal_details(file_url: str) -> Task:
     )
 
 
-def extract_work_experience(file_url: str) -> Task:
+def extract_work_experience(file_path: str) -> Task:
     return Task(
-        description=f""" Given a .pdf, .doc or .docx document provided at this url: {file_url}, read the file and extract the work experience of the applicant. Keep the original description of the work experience as well as enhance the description of the work experience by rectifying any typos, grammatical errors and sentence structure. Return the information as a JSON string with the following 
+        description=f""" Given a .pdf, .doc or .docx document provided at this file path: {file_path}, read the file and extract the work experience of the applicant. Keep the original description of the work experience as well as enhance the description of the work experience by rectifying any typos, grammatical errors and sentence structure. Return the information as a JSON string with the following 
         structure: 
         {{ 
             "work_experience": [ 
@@ -146,9 +103,9 @@ def extract_work_experience(file_url: str) -> Task:
     )
 
 
-def extract_education(file_url: str) -> Task:
+def extract_education(file_path: str) -> Task:
     return Task(
-        description=f""" Given a .pdf, .doc or .docx document provided at this url: {file_url}, read the file and extract the education history of the applicant. Keep the original description 
+        description=f""" Given a .pdf, .doc or .docx document provided at this file path: {file_path}, read the file and extract the education history of the applicant. Keep the original description 
         of the education as well as enhance the description of the education by rectifying any typos, grammatical 
         errors and sentence structure. Return the information as a JSON string with the following structure: 
         {{ 
@@ -170,12 +127,12 @@ def extract_education(file_url: str) -> Task:
     )
 
 
-def extract_qualifications(file_url: str) -> Task:
+def extract_qualifications(file_path: str) -> Task:
     return Task(
-        description=f""" Given a .pdf, .doc or .docx document provided at this url: {file_url}, read the file and extract the education history of the applicant. Return the information as a JSON 
+        description=f""" Given a .pdf, .doc or .docx document provided at this file path: {file_path}, read the file and extract the qualifications of the applicant (outside of university education). Return the information as a JSON 
         string with the following structure: 
         {{ 
-            "education": [
+            "qualifications": [
                 {{
                     "qualification": "CFA",
                     "awardedDate": "2023-02-21",
@@ -188,20 +145,9 @@ def extract_qualifications(file_url: str) -> Task:
     )
 
 
-def parse_json_output(output: str) -> Dict[str, Any]:
+def process_resume(file_url: str) -> Dict[str, Any]:
     try:
-        # Try to extract JSON from the string
-        json_start = output.index('{')
-        json_end = output.rindex('}') + 1
-        json_str = output[json_start:json_end]
-        return json.loads(json_str)
-    except (ValueError, json.JSONDecodeError):
-        return {"error": f"Failed to parse JSON from string: {output}"}
-
-
-def process_resume(file_path: str) -> Dict[str, Any]:
-    try:
-        extract_personal_details_task = extract_personal_details(file_path)
+        extract_personal_details_task = extract_personal_details(file_url)
         crew = Crew(
             agents=[personal_details_analyst],
             tasks=[extract_personal_details_task],
@@ -209,7 +155,7 @@ def process_resume(file_path: str) -> Dict[str, Any]:
         )
         personal_details = crew.kickoff()
 
-        extract_work_experience_task = extract_work_experience(file_path)
+        extract_work_experience_task = extract_work_experience(file_url)
         crew = Crew(
             agents=[work_experience_analyst],
             tasks=[extract_work_experience_task],
@@ -217,15 +163,15 @@ def process_resume(file_path: str) -> Dict[str, Any]:
         )
         work_experience = crew.kickoff()
 
-        extract_education_task = extract_education(file_path)
+        extract_education_task = extract_education(file_url)
         crew = Crew(
-            agents=[work_experience_analyst],
-            tasks=[extract_work_experience_task],
+            agents=[education_analyst],
+            tasks=[extract_education_task],
             verbose=2
         )
         education = crew.kickoff()
 
-        extract_qualifications_task = extract_qualifications(file_path)
+        extract_qualifications_task = extract_qualifications(file_url)
         crew = Crew(
             agents=[qualifications_analyst],
             tasks=[extract_qualifications_task],
@@ -244,20 +190,25 @@ def process_resume(file_path: str) -> Dict[str, Any]:
         return {"error": str(e)}
 
 
-def main():
-    result = process_resume(pdf_url)
 
-    if "error" in result:
-        print(f"An error occurred: {result['error']}")
-    else:
-        print("Personal_details:")
-        print(json.dumps(result["personal_details"], indent=2))
-        print("\nWork_experience:")
-        print(json.dumps(result["work_experience"], indent=2))
-        print("\nEducation:")
-        print(json.dumps(result["education"], indent=2))
-        print("\nQualifications:")
-        print(json.dumps(result["qualifications"], indent=2))
+def main(doc_url:str):
+    doc_content = download_doc(doc_url)
+
+    # Create a temporary file to save the downloaded PDF
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(doc_content)
+
+    try:
+        result = process_resume(temp_file.name)
+        if "error" in result:
+            print(f"An error occurred: {result['error']}")
+        else:
+            return result
+
+    finally:
+        # Clean up the temp file
+        if os.path.exists(temp_file.name):
+            os.remove(temp_file.name)
 
 
 if __name__ == "__main__":
